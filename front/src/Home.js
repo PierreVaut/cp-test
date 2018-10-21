8c4641be2c137008fa4f978dd703acd8314f77b6
Pierre Vautherin <pierre.vautherin@sfrpims.fr>
2018-10-21 22:09:15 +0200

    ADD  -  Front :  show top 10 users

--- front/src/Home.js
+++ front/src/Home.js
@@ -1,5 +1,39 @@
-import React from 'react'
+import React, { Component } from 'react';
+import request from 'superagent';
 
-const Home = () => <h4>Home....</h4>
+const urlForTopUsers = `http://localhost:8000/api/rider/loyalty/top-users`;
+
+
+class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        topUsers: []
+    };
+  }
+  
+  async componentDidMount() {
+    request
+    .get(urlForTopUsers)
+    .then(res => {
+      this.setState({
+        topUsers: res.body
+      });
+      console.log(res.body)
+    }, (err) => {
+      this.setState({
+        error: err.toString()
+      });
+    });
+  }
+
+  render() {
+      if(this.state.topUsers){
+          // TODO : add .map with topUser component
+          return <p>{JSON.stringify(this.state.topUsers)}</p>
+      }
+      return <p>Loading...</p>
+  }
+}
 
 export default Home
